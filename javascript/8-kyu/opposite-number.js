0089649fae7c7a867c6271b6cb81ae299e7d9310
Chase Peterson <belax_8@hotmail.com>
2016-07-30 22:32:25 -0600

    added kata

---
+++ javascript/8-kyu/opposite-number.js
@@ -0,0 +1,21 @@
+//Rules
+/*
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+But can you do it in 1 line of code and without any conditionals?
+*/
+
+
+//My Code
+function opposite(number) {
+  return number * -1;
+}
+
+
+//Tests
+opposite(1); //should return -1
